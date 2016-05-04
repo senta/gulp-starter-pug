2d9e5c30931c97a31f42ec7b672f54ce32509456
s.senga <1989.shota@gmail.com>
2016-05-05 01:19:23 +0900

    Add more gulp-data functionality
    Now each page can have page specified variables rather than ONE global

--- gulpfile.js/tasks/html.js
+++ gulpfile.js/tasks/html.js
@@ -1,34 +1,43 @@
-var config       = require('../config')
-if(!config.tasks.html) return
+var config       = require('../config');
+if(!config.tasks.html) return;
 
-var browserSync  = require('browser-sync')
-var data         = require('gulp-data')
-var gulp         = require('gulp')
-var gulpif       = require('gulp-if')
-var handleErrors = require('../lib/handleErrors')
-var htmlmin      = require('gulp-htmlmin')
-var path         = require('path')
-var render       = require('gulp-pug')
-var fs           = require('fs')
+var browserSync  = require('browser-sync');
+var data         = require('gulp-data');
+var gulp         = require('gulp');
+var gulpif       = require('gulp-if');
+var handleErrors = require('../lib/handleErrors');
+var htmlmin      = require('gulp-htmlmin');
+var path         = require('path');
+var render       = require('gulp-pug');
+var fs           = require('fs');
+var JSON5        = require('json5');
 
-var exclude = path.normalize('!**/{' + config.tasks.html.excludeFolders.join(',') + '}/**')
+var exclude = path.normalize('!**/{' + config.tasks.html.excludeFolders.join(',') + '}/**');
 
 var paths = {
   src: [path.join(config.root.src, config.tasks.html.src, '/**/*.' + config.tasks.html.extensions + ''), exclude],
-  dest: path.join(config.root.dest, config.tasks.html.dest),
-}
+  dest: path.join(config.root.dest, config.tasks.html.dest)
+};
 
 var getData = function(file) {
-  console.log(file); // TODO: configure to load each page variable at needs
-  var dataPath = path.resolve(config.root.src, config.tasks.html.src, config.tasks.html.dataFile)
-  return JSON.parse(fs.readFileSync(dataPath, 'utf8'))
-}
+  var dataDir = path.resolve(config.root.src, config.tasks.html.src, config.tasks.html.dataDir);
+  var templateDir = path.resolve(config.root.src, config.tasks.html.src);
+  var pageJson = file.path.substr(templateDir.length + 1).replace(new RegExp('(' + config.tasks.html.extensions.join('|')+ ')$'), 'json');
 
-var htmlTask = function() {
-  var debug = require('gulp-debug')
-  console.log(paths.src);
+  return ['global.json', pageJson].reduce(function (obj, json) {
+    try {
+      return Object.assign(obj, JSON5.parse(fs.readFileSync(path.join(dataDir, json), 'utf8')));
+    } catch (e) {
+      if (e.code !== 'ENOENT') {
+        throw e;
+      }
+    }
+    return obj;
+  }, {});
+};
+
+ var htmlTask = function() {
   return gulp.src(paths.src)
-    .pipe(debug())
     .pipe(data(getData))
     .on('error', handleErrors)
     .pipe(render({
@@ -36,12 +45,12 @@ var htmlTask = function() {
       compileDebug: !global.production,
       pretty: '  '
     }))
-    .on('error', handleErrors)
-    .pipe(gulpif(global.production, htmlmin(config.tasks.html.htmlmin)))
-    .pipe(gulp.dest(paths.dest))
-    .pipe(browserSync.stream())
+     .on('error', handleErrors)
+     .pipe(gulpif(global.production, htmlmin(config.tasks.html.htmlmin)))
+     .pipe(gulp.dest(paths.dest))
+     .pipe(browserSync.stream());
 
-}
+};
 
-gulp.task('html', htmlTask)
-module.exports = htmlTask
+gulp.task('html', htmlTask);
+module.exports = htmlTask;
